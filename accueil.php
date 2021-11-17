9a2bc3058a4a011bbd0cb9fc77895b30eb85182f
Alec <89576534+Alexia50@users.noreply.github.com>
2021-11-17 16:52:09 +0100

    Add files via upload

---
+++ accueil.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title> Site Personnel Alexia RESCH </title>
+    <metacharset="utf-10">
+    <link rel="stylesheet" type="text/css" href="contenu.css">
+</head>
+<header>
+    <div class="menu" id="bandeau">
+            <ul class="navbar-nav mr-auto">
+        <li class="active"><a href="files/a propos.php"> A PROPOS </a></li>
+        <li class="active"><a href="files/compétences.php"> COMPETENCES </a></li>
+        <li class="active"><a href="files/experience.php"> EXPERIENCE </a></li>
+        <li class="active"><a href="files/formation.php"> FORMATION</a></li>
+        <li class="active"><a href="files/contact.php"> CONTACT </a></li>
+                </ul>
+    </div>
+
+</header>
+<body>
+<h1>Site Personnel Alexia <strong> RESCH</h1>
+    <h1> Présentation </h1>
+    <h2> A la recherche de ma prochaine opportunitée </h2>
+
+
+
+##  // Visuel ou photo
+##  // Menu vers réseaux pros
+##  // Lien vers page suivante (suite)
+
+        <h3>  Menu (A propos, Compétences, Expérience, Formation, Contact)</h3>
+          <a href="https://fr.linkedin.com/in/alexia-resch-584870110"> Accès LinkedIn </a>
+
+</body>
+</html>
