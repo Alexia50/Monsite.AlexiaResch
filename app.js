b61ced7a83db63ccab49f7604ee06c61cd374928
Alec <89576534+Alexia50@users.noreply.github.com>
2022-01-17 08:57:59 +0100

    17.01

--- app.js
+++ app.js
@@ -38,3 +38,68 @@ onglets.forEach(onglet => {
     })
 
 })
+const title = document.querySelector(".containerNom h1")
+const letters = [...document.querySelectorAll('h1 span')]
+title.addEventListener("mouseenter", handleLetters);
+title.addEventListener("mouseleave", handleLetters);
+let isAnimatingIn = false;
+let calledOut = false;
+let animOpened = false;
+function handleLetters(){
+  if(animOpened){
+    animOut();
+    animOpened = false;
+    return;
+  }
+  if(isAnimatingIn){
+    calledOut = true;
+    return;
+  }
+  isAnimatingIn = true;
+  const animPromise = new Promise((resolve) => {
+    animIn()
+    setTimeout(() => {
+      resolve()
+    }, 500)
+  })
+  animPromise.then(() => {
+    isAnimatingIn = false;
+    if(calledOut) {
+      animOut()
+      calledOut = false;
+    } else if (!calledOut){
+      animOpened = true;
+    }
+
+  })
+}
+function animIn(){
+  anime({
+    targets: "h1 span",
+    translateX: function(){
+      return anime.random(-250,250)
+    },
+    translateY: function(){
+      return anime.random(-250,250)
+    },
+    translateZ: function(){
+      return anime.random(-2000,750)
+    },
+    rotate: function(){
+      return anime.random(-250,250)
+    },
+    easing: "easeOutCirc",
+    duration: 500
+  })
+}
+function animOut(){
+  anime({
+    targets: "h1 span",
+    translateX: 0,
+    translateY: 0,
+    translateZ: 0,
+    rotate: 0,
+    easing: "easeInQuad",
+    duration: 500
+  })
+}
